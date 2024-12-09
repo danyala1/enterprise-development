ba35e0caf8e98215c111596b1e9df2b1145d8a7b
danyala1 <d.mayasov@mail.ru>
2024-12-09 16:17:51 +0400

    Создал основы проектов Domain и Tests

---
+++ UniversityData/UniversityData.Domain/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace UniversityData.Domain
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ UniversityData/UniversityData.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace UniversityData.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
