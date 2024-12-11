1d0a82e394fd435c640dd169d1d73b48369fd8fa
danyala1 <d.mayasov@mail.ru>
2024-12-11 15:07:05 +0400

    Fix

--- UniversityData/UniversityData.Tests/Fixture.cs
+++ UniversityData/UniversityData.Tests/Fixture.cs
@@ -5,95 +5,62 @@ using UniversityData.Domain;
 public class UnitFixture
 {
     public List<Specialty> Specialties =>
-[
-    new() { Code = "09.03.03", Name = "Прикладная информатика" },
-    new() { Code = "09.03.02", Name = "Информационные системы и технологии" },
-    new() { Code = "09.03.01", Name = "Информатика и вычислительная техника" },
-    new() { Code = "01.03.02", Name = "Прикладная математика и информатика" },
-    new() { Code = "10.05.03", Name = "Информационная безопасность автоматизированных систем" }
-];
+    [
+        new() { Code = "09.03.03", Name = "Прикладная информатика" },
+        new() { Code = "09.03.02", Name = "Информационные системы и технологии" },
+        new() { Code = "09.03.01", Name = "Информатика и вычислительная техника" },
+        new() { Code = "01.03.02", Name = "Прикладная математика и информатика" },
+        new() { Code = "10.05.03", Name = "Информационная безопасность автоматизированных систем" }
+    ];
 
     public List<Department> Departments =>
-[
+    [
 
-    new Department { Name = "ГИиБ", SupervisorNumber = "890918734" },
-    new Department { Name = "Кафедры алгебры и геометрии", SupervisorNumber = "890918735" },
-    new Department { Name = "Кафедра высшей математики", SupervisorNumber = "890918736" },
-    new Department { Name = "Кафедра информационных технологий", SupervisorNumber = "890918737" }
-];
+        new() { Name = "ГИиБ", SupervisorNumber = "890918734" },
+        new() { Name = "Кафедры алгебры и геометрии", SupervisorNumber = "890918735" },
+        new() { Name = "Кафедра высшей математики", SupervisorNumber = "890918736" },
+        new() { Name = "Кафедра информационных технологий", SupervisorNumber = "890918737" }
+    ];
 
 
     public List<Rector> Rectors =>
-[
-    new() { Name = "Владимир", Surname = "Богатырев", Patronymic = "Дмитриевич", Degree = "Доктор экономических наук", Title = "Профессор", Position = "Ректор" },
-    new() { Name = "Дмитрий", Surname = "Быков", Patronymic = "Евгеньевич", Degree = "Доктор технических наук", Title = "Профессор", Position = "Ректор" },
-    new() { Name = "Вадим", Surname = "Ружников", Patronymic = "Александрович", Degree = "Кандидат технических наук", Title = "Доцент", Position = "Ректор" }
-];
+    [
+        new() { Name = "Владимир", Surname = "Богатырев", Patronymic = "Дмитриевич", Degree = "Доктор экономических наук", Title = "Профессор", Position = "Ректор" },
+        new() { Name = "Дмитрий", Surname = "Быков", Patronymic = "Евгеньевич", Degree = "Доктор технических наук", Title = "Профессор", Position = "Ректор" },
+        new() { Name = "Вадим", Surname = "Ружников", Patronymic = "Александрович", Degree = "Кандидат технических наук", Title = "Доцент", Position = "Ректор" }
+    ];
 
 
     public List<Faculty> Faculties =>
-[ 
-    new Faculty { Name = "Институт информатики и кибернетики", WorkersCount = 16, StudentsCount = 110 },
-    new Faculty { Name = "Институт экономики и управления", WorkersCount = 22, StudentsCount = 81 },
-    new Faculty { Name = "Юридический институт", WorkersCount = 11, StudentsCount = 65 },
-    new Faculty { Name = "Социально-гумманитарный институт", WorkersCount = 30, StudentsCount = 200 },
-    new Faculty { Name = "Институт доп. образования", WorkersCount = 22, StudentsCount = 62 },
-    new Faculty { Name = "Институт двигателей и энергетических установок", WorkersCount = 16, StudentsCount = 70 }
-];
+    [ 
+        new() { Name = "Институт информатики и кибернетики", WorkersCount = 16, StudentsCount = 110 },
+        new() { Name = "Институт экономики и управления", WorkersCount = 22, StudentsCount = 81 },
+        new() { Name = "Юридический институт", WorkersCount = 11, StudentsCount = 65 },
+        new() { Name = "Социально-гумманитарный институт", WorkersCount = 30, StudentsCount = 200 },
+        new() { Name = "Институт доп. образования", WorkersCount = 22, StudentsCount = 62 },
+        new() { Name = "Институт двигателей и энергетических установок", WorkersCount = 16, StudentsCount = 70 }
+    ];
 
 
     public List<SpecialtyTableNode> SpecialtyTableNodes =>
     [
-    new() { Specialty = Specialties[0], CountGroups = 8 },
-    new() { Specialty = Specialties[0], CountGroups = 17 },
-    new() { Specialty = Specialties[1], CountGroups = 6 },
-    new() { Specialty = Specialties[1], CountGroups = 6 },
-    new() { Specialty = Specialties[2], CountGroups = 9 },
-    new() { Specialty = Specialties[2], CountGroups = 4 },
-    new() { Specialty = Specialties[3], CountGroups = 8 },
-    new() { Specialty = Specialties[3], CountGroups = 8 },
-    new() { Specialty = Specialties[4], CountGroups = 10 },
-    new() { Specialty = Specialties[4], CountGroups = 8 },
-    new() { Specialty = Specialties[4], CountGroups = 8 }
+        new() { Specialty = Specialties[0], CountGroups = 8 },
+        new() { Specialty = Specialties[0], CountGroups = 17 },
+        new() { Specialty = Specialties[1], CountGroups = 6 },
+        new() { Specialty = Specialties[1], CountGroups = 6 },
+        new() { Specialty = Specialties[2], CountGroups = 9 },
+        new() { Specialty = Specialties[2], CountGroups = 4 },
+        new() { Specialty = Specialties[3], CountGroups = 8 },
+        new() { Specialty = Specialties[3], CountGroups = 8 },
+        new() { Specialty = Specialties[4], CountGroups = 10 },
+        new() { Specialty = Specialties[4], CountGroups = 8 },
+        new() { Specialty = Specialties[4], CountGroups = 8 }
     ];
 
     public List<University> Universities =>
-[
- new University
-    {
-        Number = "12345",
-        Name = "Самарский университет",
-        Address = "Самара",
-        RectorData = Rectors[0],
-        UniversityProperty = "муниципальная",
-        ConstructionProperty = "муниципальная",
-        FacultiesData = Faculties.Take(3).ToList(),
-        DepartmentsData = Departments.Take(2).ToList(),
-        SpecialtyTable = SpecialtyTableNodes.Take(3).ToList()
-    },
-    new University
-    {
-        Number = "56789",
-        Name = "СамГТУ",
-        Address = "Самара",
-        RectorData = Rectors[1],
-        UniversityProperty = "муниципальная",
-        ConstructionProperty = "муниципальная",
-        FacultiesData = Faculties.Skip(3).Take(2).ToList(),
-        DepartmentsData = Departments.Skip(2).Take(1).ToList(),
-        SpecialtyTable = SpecialtyTableNodes.Skip(3).Take(4).ToList()
-    },
-    new University
-    {
-        Number = "45678",
-        Name = "ПГУТИ",
-        Address = "Самара",
-        RectorData = Rectors[2],
-        UniversityProperty = "муниципальная",
-        ConstructionProperty = "федеральная",
-        FacultiesData = Faculties.Skip(5).Take(1).ToList(),
-        DepartmentsData = Departments.Skip(3).Take(1).ToList(),
-        SpecialtyTable = SpecialtyTableNodes.Skip(7).Take(4).ToList()
-    }
-];     
+    [
+        new() { Number = "12345", Name = "Самарский университет", Address = "Самара", RectorData = Rectors[0], UniversityProperty = "муниципальная", ConstructionProperty = "муниципальная", FacultiesData = Faculties.Take(3).ToList(), DepartmentsData = Departments.Take(2).ToList(), SpecialtyTable = SpecialtyTableNodes.Take(3).ToList() },
+        new() { Number = "56789", Name = "СамГТУ", Address = "Самара", RectorData = Rectors[1], UniversityProperty = "муниципальная", ConstructionProperty = "муниципальная", FacultiesData = Faculties.Skip(3).Take(2).ToList(), DepartmentsData = Departments.Skip(2).Take(1).ToList(), SpecialtyTable = SpecialtyTableNodes.Skip(3).Take(4).ToList() },
+        new() { Number = "45678", Name = "ПГУТИ", Address = "Самара", RectorData = Rectors[2], UniversityProperty = "муниципальная", ConstructionProperty = "федеральная", FacultiesData = Faculties.Skip(5).Take(1).ToList(), DepartmentsData = Departments.Skip(3).Take(1).ToList(), SpecialtyTable = SpecialtyTableNodes.Skip(7).Take(4).ToList() }
+    ];     
 }
--- UniversityData/UniversityData.Tests/Tests.cs
+++ UniversityData/UniversityData.Tests/Tests.cs
@@ -3,7 +3,7 @@ using System.Linq;
 
 public class UnitTests(UnitFixture unitFixture) : IClassFixture<UnitFixture>
 {
-    private readonly UnitFixture _fixture = unitFixture;
+    private readonly UnitFixture unitFixture = unitFixture;
     /// <summary>
     /// Запрос 1 - Вывести информацию о выбранном вузе.
     /// </summary>
@@ -11,7 +11,7 @@ public class UnitTests(UnitFixture unitFixture) : IClassFixture<UnitFixture>
     public void InformationOfUniversity()
     {
         string universityNumber = "12345";
-        var result = (from university in _fixture.Universities
+        var result = (from university in unitFixture.Universities
                       where university.Number == universityNumber
                       select university).ToList();
         Assert.Equal("Самарский университет", result[0].Name);
@@ -23,7 +23,7 @@ public class UnitTests(UnitFixture unitFixture) : IClassFixture<UnitFixture>
     public void InformationOfStructure()
     {
         string universityNumber = "45678";
-        var result = (from university in _fixture.Universities
+        var result = (from university in unitFixture.Universities
                       where university.Number == universityNumber
                       select university).ToList();
         Assert.Single(result[0].FacultiesData);
@@ -38,7 +38,7 @@ public class UnitTests(UnitFixture unitFixture) : IClassFixture<UnitFixture>
     {
         var expectedSpecialties = new List<string> { "10.05.03", "09.03.03", "09.03.02", "09.03.01", "01.03.02" };
 
-        var result = (from specialtyNode in _fixture.SpecialtyTableNodes
+        var result = (from specialtyNode in unitFixture.SpecialtyTableNodes
                       let specialtyCode = specialtyNode.Specialty?.Code
                       where specialtyCode != null
                       group specialtyNode by specialtyCode into specialtyGroup
@@ -50,7 +50,6 @@ public class UnitTests(UnitFixture unitFixture) : IClassFixture<UnitFixture>
         {
             Assert.Equal(expectedSpecialties[i], result[i]);
         }
-
     }
     /// <summary>
     /// Запрос 4 - Вывести информацию о ВУЗах с максимальным количеством кафедр, упорядочить по названию.
@@ -58,8 +57,8 @@ public class UnitTests(UnitFixture unitFixture) : IClassFixture<UnitFixture>
     [Fact]
     public void MaxCountDepartments()
     {
-        var result = (from university in _fixture.Universities
-                      where university.DepartmentsData.Count == _fixture.Universities.Max(x => x.DepartmentsData.Count)
+        var result = (from university in unitFixture.Universities
+                      where university.DepartmentsData.Count == unitFixture.Universities.Max(x => x.DepartmentsData.Count)
                       select university).ToList();
         Assert.Single(result);
     }
@@ -72,11 +71,11 @@ public class UnitTests(UnitFixture unitFixture) : IClassFixture<UnitFixture>
         string universityType = "муниципальная";
         int requiredCountGroups = 27;
 
-        var result = (from university in _fixture.Universities
+        var result = (from university in unitFixture.Universities
                       where university.UniversityProperty == universityType
                       where university.SpecialtyTable.Sum(x => x.CountGroups) == requiredCountGroups
                       select university).ToList();
-        Assert.Equal(1, result.Count);
+        Assert.Single(result);
     }
     /// <summary>
     /// Запрос 6 - Вывести информацию о количестве факультетов, кафедр, специальностей по каждому типу собственности учреждения и собственности здания.
@@ -84,23 +83,22 @@ public class UnitTests(UnitFixture unitFixture) : IClassFixture<UnitFixture>
     [Fact]
     public void CountDepartments()
     {
-        var result = (from university in _fixture.Universities
+        var result = (from university in unitFixture.Universities
                       group university by university.ConstructionProperty into universityConstGroup
                       select new
                       {
                           ConstProp = universityConstGroup.Key,
                           UniversityProperties = (from university in universityConstGroup
-                                                  group university by university.UniversityProperty into universityPropGroup
-                                                  select new
-                                                  {
-                                                      UniversityProp = universityPropGroup.Key,
-                                                      Faculties = universityConstGroup.Sum(x => x.FacultiesData.Count),
-                                                      Departments = universityConstGroup.Sum(x => x.DepartmentsData.Count),
-                                                      Specialities = universityConstGroup.Sum(x => x.SpecialtyTable.Count)
-                                                  }).ToList()
+                          group university by university.UniversityProperty into universityPropGroup
+                          select new
+                          {
+                            UniversityProp = universityPropGroup.Key,
+                            Faculties = universityConstGroup.Sum(x => x.FacultiesData.Count),
+                            Departments = universityConstGroup.Sum(x => x.DepartmentsData.Count),
+                            Specialities = universityConstGroup.Sum(x => x.SpecialtyTable.Count)
+                          }).ToList()
                       }).ToList();
 
-        // Проверка результатов
         Assert.Equal("муниципальная", result[0].UniversityProperties[0].UniversityProp);
         Assert.Equal("муниципальная", result[1].UniversityProperties[0].UniversityProp);
         Assert.Equal("муниципальная", result[0].ConstProp);
@@ -111,6 +109,5 @@ public class UnitTests(UnitFixture unitFixture) : IClassFixture<UnitFixture>
         Assert.Equal(1, result[1].UniversityProperties[0].Departments);
         Assert.Equal(7, result[0].UniversityProperties[0].Specialities);
         Assert.Equal(4, result[1].UniversityProperties[0].Specialities);
-
     }
 }
